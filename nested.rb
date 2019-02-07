3b374479b75f6184b89a1f4c5c8efddfaffce842
Louie O'Grady <l.ogrady@gmail.com>
2019-02-07 14:57:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     dennis_ritchies_language[:dennis_ritchie][:dennis_ritchies_language]
+     dennis_ritchies_language[:dennis_ritchie][:languages]
 end
 
 def adding_matz
