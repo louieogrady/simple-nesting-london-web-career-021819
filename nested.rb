ed30693272f3f2bb1622beeee76cdd84134bbe76
Louie O'Grady <l.ogrady@gmail.com>
2019-02-07 15:22:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
 end
 
 def changing_alan
@@ -105,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
