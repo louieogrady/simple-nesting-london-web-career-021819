ed28c8b5f73c8ab8bf922e3075c9546307a35fa2
Louie O'Grady <l.ogrady@gmail.com>
2019-02-07 15:25:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay].delete(:known_for)
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash[:alan_kay].delete(:age)
 end
 
 def adding_to_dennis
